f979fcebf150f11b094aaa8dfb8a0831c5f252ce
OBNA <obna.adeleke@gmail.com>
2021-01-31 21:02:48 -0600

    Add js for nav bar visibility

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-//JS
+const bodyElement = document.querySelector('body');
+const menuToggle = document.querySelector('.hamburger-menu');
+
+menuToggle.addEventListener('click', function(){
+    bodyElement.classList.toggle('is-open');
+});
